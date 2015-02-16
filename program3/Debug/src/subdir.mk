c77350ba1db3bac1a3f1e96b15f30b505dc75e19
Shaun McThomas <smcthoma@uci.edu>
2015-02-15 16:48:30 -0800

    Revert this to run on Ian PC.
    Had to change these two files in order to get make file to run on my PC.
    If you revert this change, you should be able to run on your PC again.

--- program3/Debug/src/subdir.mk
+++ program3/Debug/src/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"C:\Users\Ian\workspace\courselib\src" -I"C:\Users\Ian\workspace\googletestlib\include" -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"C:\Users\Shaun\OneDrive\Eclipse\workspace\courselib\src" -I"C:\Users\Shaun\OneDrive\Eclipse\workspace\googletestlib\include" -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
