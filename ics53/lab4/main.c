12d884d0d523556a620d507ffe2de3644e511bc3
Ian Geoffrey Schweer <ischweer@mannfred-uta.ics.uci.edu>
2015-06-03 02:27:50 -0700

    Will make multiple request and block on blocked requests, writes to log

--- ics53/lab4/main.c
+++ ics53/lab4/main.c
@@ -33,8 +33,8 @@ int parse_uri(char *uri, char *target_addr, char *path, int  *port);
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
 
 int get_uri_size(char*);
-void read_response_stream(int, FILE*, int, char*);
-void read_whole_stream(int, FILE*, char*);
+int read_response_stream(int, int, char*, char*, char*, int);
+void read_whole_stream(int, int, rio_t*, char*);
 int ensure_get_version(char*);
 void process_req();
 void print_this_ip();
@@ -79,10 +79,11 @@ int main(int argc, char** argv) {
 
 void process_req(){
 	//Prefer to work with FILE
-	FILE *client_f = fdopen(appdat->client_fd,"r");
-	char buffer[MAXLINE], request[MAXLINE], response[MAXLINE], hostname[MAXLINE], path[MAXLINE];
+	int client_fd = appdat->client_fd;
+	char buffer[MAXLINE], request[MAXLINE], response[MAXLINE], hostname[MAXLINE], path[MAXLINE], log[MAXLINE];
 	char *request_uri, *host_address_ptr, *rest;
 	int size = -1, port;
+	rio_t rio;
 	struct sockaddr_in clientaddr = appdat->clientaddr;
 	struct hostent *host_ptr;
 	
@@ -94,15 +95,17 @@ void process_req(){
 			   AF_INET);
 	host_address_ptr = inet_ntoa(clientaddr.sin_addr);
 	
-	read_whole_stream(MAXLINE, client_f, request);
+	Rio_readinitb(&rio, client_fd);
+	read_whole_stream(MAXLINE, client_fd, &rio, request);
 	
 	// We don't handle POST, PUT, or DELETE requests.
 	if (strncmp(request, "GET ", strlen("GET "))) {
 		printf("process_request: Received non-GET request\n");
-		fclose(client_f);
+		close(client_fd);
+		request[0] = '\0';
 		return;
 	}
-	
+	printf("REQUEST:\n---------------------------------- \n%s\n----------------------------------\n", request);
 	// filter out the "GET " from the request
 	request_uri = request + 4;
 	size = get_uri_size(request_uri);
@@ -110,26 +113,29 @@ void process_req(){
 		printf("Not proper GET version");
 		return;
 	}
-	
-	rest = &request_uri[size + 1];
+	rest = &request_uri[size + 1]  + strlen("HTTP/1.0\r\n");
 	// we have a good url, lets parse it and get our end data.
 	if (parse_uri(request_uri, hostname, path, &port) < 0) {
 		printf("Failed to parse url %s", request_uri);
 		return;
 	}
 	else {
-		int n = 0, response_len = 0, server_fd = open_clientfd(hostname, port);
+		int n = read_response_stream(client_fd, MAXLINE, hostname, path, rest, port);
+		format_log_entry(log, &clientaddr, request_uri, n);
 		
-		rio_writen(server_fd, "GET /", strlen("GET /"));
-		rio_writen(server_fd, path, strlen(path));
-		rio_writen(server_fd, " HTTP/1.0\r\n", strlen(" HTTP/1.0\r\n"));
-		rio_writen(server_fd, rest, strlen(rest));
-		
-		read_response_stream(server_fd, client_f, MAXLINE, response);
+		// log
+		FILE *logf = Fopen(LOG, "a");
+		fprintf(logf, "%s %d\n", log, n);
+		fflush(logf);
+		fclose(logf);
 	}
 	//close connection
-	fclose(client_f);
 	close(appdat->client_fd);
+	request[0] = '\0';
+	response[0] = '\0';
+	hostname[0] = '\0';
+	path[0] = '\0';
+	return;
 }
 
 //debug function to print ip
@@ -172,8 +178,8 @@ int parse_uri(char *uri, char *hostname, char *pathname, int *port)
     int len;
 
     if (strncasecmp(uri, "http://", 7) != 0) {
-	hostname[0] = '\0';
-	return -1;
+		hostname[0] = '\0';
+		return -1;
     }
        
     /* Extract the host name */
@@ -186,24 +192,24 @@ int parse_uri(char *uri, char *hostname, char *pathname, int *port)
     /* Extract the port number */
     *port = 80; /* default */
     if (*hostend == ':')   
-	*port = atoi(hostend + 1);
+		*port = atoi(hostend + 1);
     
     /* Extract the path */
     pathbegin = strchr(hostbegin, '/');
     if (pathbegin == NULL) {
-	pathname[0] = '\0';
+		pathname[0] = '\0';
     }
     else {
-	pathbegin++;	
-	strcpy(pathname, pathbegin);
+		pathbegin++;	
+		strcpy(pathname, pathbegin);
     }
 
     return 0;
 }
 
-void read_whole_stream(int size, FILE *stream, char *response) {
+void read_whole_stream(int size, int stream, rio_t *rio, char *response) {
 	char buffer[size];
-	while (fgets(buffer,size,stream) != NULL) {
+	while (rio_readlineb(rio,buffer,size) > 0) {
 		// copy.
 		strcat(response, buffer);
 		if (strcmp(buffer, "\r\n") == 0)
@@ -211,15 +217,54 @@ void read_whole_stream(int size, FILE *stream, char *response) {
 	}
 }
 
-void read_response_stream(int server_fd, FILE *client_f, int size, char *response) {
+int read_response_stream(int client_f, int size, char *hostname, char *path, char *rest, int port) {
 	rio_t rio;
-    int response_len = 0, n;
+    int response_len, n, server_fd;
 	char buffer[size];
+	
+	server_fd = open_clientfd(hostname, port);		
+	rio_writen(server_fd, "GET /", strlen("GET /"));
+	rio_writen(server_fd, path, strlen(path));
+	rio_writen(server_fd, " HTTP/1.0\r\n", strlen(" HTTP/1.0\r\n"));
+	rio_writen(server_fd, rest, strlen(rest));
+	fflush(stdout);
 	Rio_readinitb(&rio, server_fd);
     while( (n = rio_readn(server_fd, buffer, size)) > 0 ) {
 		response_len += n;
-		fprintf(client_f, buffer, n);
+		printf("Recieved: %d\n", n);
+		fflush(stdout);
+		rio_writen(client_f, buffer, n);
 		bzero(buffer, MAXLINE);
     }
 	close(server_fd);
+	return response_len;
+}
+
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, 
+		      char *uri, int size)
+{
+    time_t now;
+    char time_str[MAXLINE];
+    unsigned long host;
+    unsigned char a, b, c, d;
+
+    /* Get a formatted time string */
+    now = time(NULL);
+    strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+
+    /* 
+     * Convert the IP address in network byte order to dotted decimal
+     * form. Note that we could have used inet_ntoa, but chose not to
+     * because inet_ntoa is a Class 3 thread unsafe function that
+     * returns a pointer to a static variable (Ch 13, CS:APP).
+     */
+    host = ntohl(sockaddr->sin_addr.s_addr);
+    a = host >> 24;
+    b = (host >> 16) & 0xff;
+    c = (host >> 8) & 0xff;
+    d = host & 0xff;
+
+
+    /* Return the formatted log entry string */
+    sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
 }
