9fed02c79ea62374f4ffff5ad58176e2542cd3be
Ian Schweer <schweer@adobe.com>
2015-09-30 15:37:32 -0700

    Added 161

---
+++ cs161/hw1.py
@@ -0,0 +1,19 @@
+import math
+i = 2
+# when is n^2 > 10nlogn
+while i > 1:
+	if (i**2) > (10 * i * (math.log(i))):
+		print i;
+		break;
+	i += 1
+
+#when is nlogn = 10^6
+while i > 1:
+	if i * math.log(i, 2) > 10**6:
+		print str(i - 1) + " < x < " + str(i)
+		break
+	elif i * math.log(i, 2) == 10**6:
+		print "x == " + str(i)
+		break
+	else:
+		i += 1
