a2e68f364deaf5a0d7d7ba0b1bdf9ac9775cd1d3
Ian Schweer <schweer@adobe.com>
2015-10-20 12:53:33 -0700

    + Args parsing correctly

--- cs143A/project3/src/myshell/my_shell.c
+++ cs143A/project3/src/myshell/my_shell.c
@@ -15,48 +15,63 @@ typedef struct state {
 	int ready;
 } state;
 
-void processCmd(char *rawData, int n) {
+void clearSpaces(char **word) {
+	while (*word[0] == ' ') *word+=1;	
+}
+
+void processCmd(char *rawData, int n, char *env[]) {
 	if (n == 0 && rawData == NULL) {
 		exit(0);
 	}
 	// make sure we don't have spaces.
-	while (rawData[0] == ' ') rawData+=1;
+	clearSpaces(&rawData);
 	
 	// data is good, do stuff with it.
 	states_t currentState = CMD;
 	state dataParsed[2];
 	int sanity = 0;
-	while (rawData[sanity] != ' ' && rawData[sanity] != '-') { sanity++; };
+	char args[BUFSIZ][BUFSIZ];
+	while (rawData[sanity] != ' ' && rawData[sanity] != '-' && sanity < n) { sanity++; };
 	memcpy(dataParsed[0].data, rawData, sanity);
 	dataParsed[0].data[sanity] = '\0';
 
 	rawData += sanity;
-	memcpy(dataParsed[1].data, rawData, n - sanity);
-	dataParsed[1].data[n - sanity] = '\0';
-	printf("CMD: %s\nARG: %s\n", dataParsed[0].data, dataParsed[1].data);	
+	if (sanity == n) {
+		printf("CMD: %s\n", dataParsed[0].data);
+		execvpe(dataParsed[0].data, NULL, env);
+	} else {
+		// erase all spaces at the beginning.
+		clearSpaces(&rawData);
+		memcpy(dataParsed[1].data, rawData, n - sanity);
+		dataParsed[1].data[n - sanity] = '\0';
+		printf("CMD: %s\nARG: %s\n", dataParsed[0].data, dataParsed[1].data);	
 
-	char args[BUFSIZ][BUFSIZ];
-	char *follower = dataParsed[1].data;
-	int i = 0, pos = 0, maxlen = n - sanity;
-	// find the first character that is not a dash or space;
-	while (dataParsed[1].data[i] != ' ' && dataParsed[1].data[i] != '-') i++;
-	i++;
+		char *follower = dataParsed[1].data;
+		int i = 0, pos = 0, maxlen = n - sanity;
+		// find the first character that is not a dash or space;
+		while (dataParsed[1].data[i] != '-' && i < maxlen) i++;
+		i = i == maxlen ? 0 : i+1;
 
-	// get all our arguments seperate for execvpe
-	while (i < maxlen) {
-		if (follower[i] == ' ' || follower[i] == '-') {
-			memcpy(args[pos], follower, i);
-			follower += i;
-			pos+=1;
-			maxlen = i; 
-			i = 0;
+		// get all our arguments seperate for execvpe
+		while (i < maxlen) {
+			if (follower[i] == ' ' || follower[i] == '-') {
+				memcpy(args[pos], follower, i);
+				follower += follower[i] == '-' ? i : i + 1;
+				pos+=1;
+				maxlen = i; 
+				i = 0;
+			}
+			i++;
 		}
-		i++;
+		memcpy(args[pos++], follower, i);
+		while (pos >= 0) {
+			printf("args[%d]: %s\n", pos, args[pos]);
+			pos--;
+		}
 	}
-	memcpy(args[pos++], follower, i);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char *env[]) {
 	printf("$: ");
 	char c = 'a', cmd[BUFSIZ];
 	int i = 0;
@@ -64,6 +79,6 @@ int main(int argc, char **argv) {
 		c = getchar();
 		cmd[i++] = c;
 	}
-	cmd[i] = '\0';
-	processCmd(cmd, i);
+	cmd[i - 1] = '\0';
+	processCmd(cmd, i - 1, env);
 }
