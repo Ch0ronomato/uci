dd10a543d346087195f997b3a898d5a593e3a620
Ian Geoffrey Schweer <ischweer@ray-gillette.ics.uci.edu>
2015-10-21 11:44:29 -0700

    + Finishd shell

--- cs143A/project3/src/myshell/my_shell.c
+++ cs143A/project3/src/myshell/my_shell.c
@@ -4,19 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 
-typedef enum states {
-	CMD,
-	ARGS
-} states_t;
-
-typedef struct state {
-	states_t state;
-	char data[BUFSIZ];
-	int ready;
-} state;
-
 void clearSpaces(char **word) {
-	while (*word[0] == ' ') *word+=1;	
+	while (*word[0] == ' ' && *word[0] != '\n') *word+=1;	
+}
+int clearSpacesC(char **word) {
+	int c = 0;
+	while (*word[0] == ' ' && *word[0] != '\n') *word+=1, c++;
+	return c;	
 }
 
 void processCmd(char *rawData, int n, char *env[]) {
@@ -24,61 +18,144 @@ void processCmd(char *rawData, int n, char *env[]) {
 		exit(0);
 	}
 	// make sure we don't have spaces.
+	rawData[n] = 0;
 	clearSpaces(&rawData);
-	
+		
 	// data is good, do stuff with it.
-	states_t currentState = CMD;
-	state dataParsed[2];
+	char first[BUFSIZ], second[BUFSIZ], third[BUFSIZ], fourth[BUFSIZ]; // two args and flags
 	int sanity = 0;
-	char args[BUFSIZ][BUFSIZ];
 	while (rawData[sanity] != ' ' && rawData[sanity] != '-' && sanity < n) { sanity++; };
-	memcpy(dataParsed[0].data, rawData, sanity);
-	dataParsed[0].data[sanity] = '\0';
+	rawData[sanity] = '\0';
+	strcpy(first, rawData);
 
-	rawData += sanity;
+	rawData += sanity+1;
 	if (sanity == n) {
-		printf("CMD: %s\n", dataParsed[0].data);
-		execvpe(dataParsed[0].data, NULL, env);
+		char *noargs[] = {first, NULL};
+		execvpe(noargs[0], noargs, env);
 	} else {
 		// erase all spaces at the beginning.
+		int remainder = n - sanity;
 		clearSpaces(&rawData);
-		memcpy(dataParsed[1].data, rawData, n - sanity);
-		dataParsed[1].data[n - sanity] = '\0';
-		printf("CMD: %s\nARG: %s\n", dataParsed[0].data, dataParsed[1].data);	
-
-		char *follower = dataParsed[1].data;
-		int i = 0, pos = 0, maxlen = n - sanity;
-		// find the first character that is not a dash or space;
-		while (dataParsed[1].data[i] != '-' && i < maxlen) i++;
-		i = i == maxlen ? 0 : i+1;
-
-		// get all our arguments seperate for execvpe
-		while (i < maxlen) {
-			if (follower[i] == ' ' || follower[i] == '-') {
-				memcpy(args[pos], follower, i);
-				follower += follower[i] == '-' ? i : i + 1;
-				pos+=1;
-				maxlen = i; 
-				i = 0;
+		if (rawData[0] == '-' || rawData[0] != ' ') {
+			// handle the flag
+			int pos = 0, i = 0;
+			while (rawData[i] != ' ' && i < remainder) {
+				second[i] = rawData[i];
+				i++;
+			}	
+			rawData += i+1;
+			remainder -= i + 1;
+			remainder -= clearSpacesC(&rawData);
+			if (rawData[0] == '\n' || rawData[0] == '\0') {
+				char *args[] = {first, second, NULL};
+				execvpe(args[0], args, env);
+				printf("Could not execute command: %s\n", args[0]);
+				exit(0);	
 			}
+			if (rawData[0] != ' ') {
+				i = 0;
+				pos++;
+				while (rawData[i] != ' ' && i < remainder) {
+					third[i] = rawData[i];
+					i++;	
+				}
+				rawData += i + 1;
+				remainder -= i + 1;
+				remainder -= clearSpacesC(&rawData);
+			} // argument three
+			else {
+				char *args[] = {first, second, NULL};
+				execvpe(args[0], args, env);
+				printf("Could not execute command: %s\n", args[0]);
+				exit(0);	
+			}
+
+			if (rawData[0] != ' ' && rawData[0] != '\0') {
+				i = 0;
+				pos++;
+				while(rawData[i] != ' ' && i < remainder) {
+					fourth[i] = rawData[i];
+					i++;
+				}
+				char *args[] = {first, second, third, fourth, NULL};
+				execvpe(args[0], args, env);
+				printf("Could not execute command: %s\n", args[0]);
+				exit(0);	
+			} // argument four
+			else {
+				char *args[] = {first, second, third, NULL};
+				execvpe(args[0], args, env);
+				printf("Could not execute command: %s\n", args[0]);
+				exit(0);	
+			}
+		}
+		else if (rawData[0] != ' ') {
+				int i = 0, pos = 0;
+				while (rawData[i] != ' ' && rawData[i] != '\n') {
+					third[i] = rawData[i];
+					i++;	
+				}
+				rawData += i + 1;
+				clearSpaces(&rawData);
+				if (rawData[0] != ' ') {
+					i = 0;
+					pos++;
+					while(rawData[i] != ' ' && rawData[i] != '\n') {
+						fourth[i] = rawData[i];
+						i++;
+					}
+					char *args[] = {first, second, third, fourth, NULL};
+					execvpe(args[0], args, env);
+					printf("Could not execute command: %s\n", args[0]);
+					exit(0);	
+				} else {
+					char *args[] = {first, second, third, NULL};
+					execvpe(args[0], args, env);
+					printf("Could not execute command: %s\n", args[0]);
+					exit(0);	
+				}
+		}
+		/*int c = 0, i = 0, pos = 0;
+		char *args[BUFSIZ];
+		rawData[n - sanity + 1] = ' ';
+		s[pos++] = first;
+		while (*(rawData + i) != '\n') {{
+			char arg[BUFSIZ] = {'\0'};
+			int j = 0;
+			while (*(rawData + i) != ' ') {  
+				*(arg + j) = *(rawData + i); 
+				j++;
+				i++;
+			}
+			*(arg + j) = '\0'; 
+			strcpy(args[pos++], arg);
 			i++;
-		}
-		memcpy(args[pos++], follower, i);
-		while (pos >= 0) {
-			printf("args[%d]: %s\n", pos, args[pos]);
-			pos--;
-		}
+		}}
+		int a = 0;*/
 	}
 }
 
 int main(int argc, char **argv, char *env[]) {
-	printf("$: ");
 	char c = 'a', cmd[BUFSIZ];
+	memset(cmd, '\0', sizeof(cmd));
 	int i = 0;
-	while (c != '\n') {
-		c = getchar();
-		cmd[i++] = c;
+	while (1) {
+		printf("$: ");
+		while (c != '\n' && c != EOF) {
+			c = getchar();
+			if (c == EOF) break;
+			memcpy(&cmd[i++], &c, sizeof(char));
+		}
+		if (cmd[0] != '\0' && cmd[0] != '\n') {
+			pid_t id = fork();
+			if (id == 0 && c != EOF)
+				processCmd(&cmd[0], i - 1, env);
+			waitpid(id, NULL, 0);
+		}
+		if (c == EOF)
+			exit(0);
+		memset(cmd, 0, sizeof(cmd));
+		i = 0;
+		c = 'a';
 	}
-	cmd[i - 1] = '\0';
-	processCmd(cmd, i - 1, env);
 }
