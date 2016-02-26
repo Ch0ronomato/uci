52bd37cd3a69b858b946602e911f4d6e46fed157
Ian Geoffrey Schweer <ischweer@woodhouse.ics.uci.edu>
2016-02-26 15:05:54 -0800

    Fixed palindrome issue

--- cs131/project3/lab3.cpp
+++ cs131/project3/lab3.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 #include <omp.h>
+#include <chrono>
+#include <thread>
 
 // Don't CHANGE This Code (you can add more functions)-----------------------------------------------------------------------------
 
@@ -56,25 +58,77 @@ DoOutput(Result r)
 }
 
 // CHANGE This Code (you can add more functions)-----------------------------------------------------------------------------
+int findLongest(std::string text, int &s) {
+    std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+    int N = (int)text.length();
+    N = 2*N + 1; //Position count
+    int L[N]; //LPS Length Array
+    L[0] = 0;
+    L[1] = 1;
+    int C = 1; //centerPosition
+    int R = 2; //centerRightPosition
+    int i = 0; //currentRightPosition
+    int iMirror; //currentLeftPosition
+    int maxLPSLength = 0;
+    int maxLPSCenterPosition = 0;
+    int start = -1;
+    int end = -1;
+    int diff = -1;
+    
+    //Uncomment it to print LPS Length array
+    //printf("%d %d ", L[0], L[1]);
+    for (i = 2; i < N; i++)
+    {
+        //get currentLeftPosition iMirror for currentRightPosition i
+        iMirror  = 2*C-i;
+        L[i] = 0;
+        diff = R - i;
+        //If currentRightPosition i is within centerRightPosition R
+        if(diff > 0)
+            L[i] = std::min(L[iMirror], diff);
+        
+        //Attempt to expand palindrome centered at currentRightPosition i
+        //Here for odd positions, we compare characters and
+        //if match then increment LPS Length by ONE
+        //If even position, we just increment LPS by ONE without
+        //any character comparison
+        while ( ((i + L[i]) < N && (i - L[i]) > 0) &&
+               ( ((i + L[i] + 1) % 2 == 0) ||
+                (text[(i + L[i] + 1)/2] == text[(i - L[i] - 1)/2] )))
+        {
+            L[i]++;
+        }
+        
+        if(L[i] > maxLPSLength)  // Track maxLPSLength
+        {
+            maxLPSLength = L[i];
+            maxLPSCenterPosition = i;
+        }
+        
+        //If palindrome centered at currentRightPosition i
+        //expand beyond centerRightPosition R,
+        //adjust centerPosition C based on expanded palindrome.
+        if (i + L[i] > R)
+        {
+            C = i;
+            R = i + L[i];
+        }
+    }
+    start = (maxLPSCenterPosition - maxLPSLength)/2;
+    end = start + maxLPSLength - 1;
+    s = start;
+    return end - start + 1;
+}
+
 Result
 findPalindrome(std::string s, int l)
 {
     Result r{0,0,0};   
-    int mid = floor(s.length() / 2);
-    int offset = mid & 1;
-    while (mid >= 0)
-    {
-        int j = 0;
-        for (int i = mid + offset + 1; i < s.length(); i++) 
-        {
-            if (s[mid - j] == s[i])
-            {
-                Result r2 {l, mid-j, i - mid - j + 1};
-                r = r < r2 ? r2 : r;
-            }
-        }
-        mid--;
-    }
+    int start;
+    int len = findLongest(s, start);
+    r.lineNumber = l;
+    r.firstChar = start;
+    r.length = len;
     return r;
 }
 
@@ -88,33 +142,33 @@ main(int argc, char* argv[])
     }
     // ....... Your OpenMP program goes here ............
     // Probably some common code...
+    std::chrono::time_point<std::chrono::system_clock> start, end;
     int th_id, nthreads, num=std::atoi(argv[2]);
     std::ifstream ifs(argv[1]); 
     std::array<std::string, INPUT_SIZE> lines = strip(ifs);
     Result resultA(0,0,0);
-
+    
     // Part A
+    start = std::chrono::system_clock::now();
     #pragma omp parallel for schedule(static) private(th_id) shared(nthreads) shared(resultA) num_threads(num)
     for (int i = 0; i < INPUT_SIZE; i++) 
     {
         Result r = findPalindrome(lines[i], i);
         resultA = resultA < r ? r : resultA;
     }
-    std::cout << "Palindrome: " << lines[resultA.lineNumber] << std::endl;
 
     // ... Eventually.. 
     DoOutput(resultA);
 
     // Part B
     Result resultB(0,0,0);
-    #pragma omp parallel for schedule(dynamic, 1) private(th_id) shared(nthreads) shared(resultB) num_threads(num)
+    start = std::chrono::system_clock::now();
+    #pragma omp parallel for schedule(dynamic, 1000) private(th_id) shared(nthreads) shared(resultB) num_threads(num)
     for (int i = 0; i < INPUT_SIZE; i++) 
     {
         Result r = findPalindrome(lines[i], i);
         resultB = resultB < r ? r : resultB;
     }
-    std::cout << "Palindrome: " << lines[resultB.lineNumber] << std::endl;
-
     // ... Eventually.. 
     DoOutput(resultB);
 
