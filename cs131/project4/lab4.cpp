1b33be0fd7495f13229d93caf1680a6f3806f4bc
Ian Geoffrey Schweer <ischweer@odin.ics.uci.edu>
2016-03-09 23:22:28 -0800

    Finished lab4 code

--- cs131/project4/lab4.cpp
+++ cs131/project4/lab4.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <fstream>
 
 // Don't CHANGE This Code (you can add more functions)-----------------------------------------------------------------------------
 
@@ -72,6 +73,28 @@ PrintElectionResult(int winnerMPIRank, int round, int numberOfProcesses, int ele
 }
 
 // CHANGE This Code (you can add more functions)-----------------------------------------------------------------------------
+int getActiveNode(int direction, int idx, int total, int election[][2]) {
+    int neighbor = -1;
+    bool done = false;
+    for (int i = 0; i < total && !done; i++) {
+        int index = (idx + (i * direction)) % total;
+        if (index < 0) index = total + index;
+        if (index != idx && election[index][1] == 1) {
+            neighbor = index;    
+            done = true;
+        }
+    }
+    return neighbor;
+}
+int 
+getLastNode(int numProcesses, int myProcessId, int election[][2]) { 
+    return getActiveNode(-1, myProcessId, numProcesses, election);
+}
+
+int
+getNextNode(int numProcesses, int myProcessId, int election[][2]) {
+    return getActiveNode(1, myProcessId, numProcesses, election);
+}
 
 int
 main(int argc, char* argv[])
@@ -100,6 +123,7 @@ main(int argc, char* argv[])
     std::srand(seed); // Set the seed
 
     auto electionArray = new int[numberOfProcesses][2]; // Bcast with &electionArray[0][0]...
+    std::ofstream f("output-" + std::to_string(processId));
 
     for(int round = 0; round < numberOfRounds; ++round)
     {
@@ -107,15 +131,97 @@ main(int argc, char* argv[])
             CreateElectionArray(numberOfProcesses, electionArray);
     
         // broadcast ElectionArray from master to all other nodes.
-        
+        MPI_Bcast(&electionArray[0][0], numberOfProcesses*2, MPI_INT, 0, MPI_COMM_WORLD);
+        int next = getNextNode(numberOfProcesses, processId, electionArray);
+        int last = getLastNode(numberOfProcesses, processId, electionArray);
+        bool active = electionArray[processId][1];
+        int lastSeen = -1;
+        f << "next=" << next << ", last=" << last << std::endl;
+        // do the election
+        int activeCount = 0, send[numberOfProcesses], recv[numberOfProcesses];
+        for (int i = 0; i < numberOfProcesses; i++) {
+            int electionId = electionArray[i][0];
+            activeCount += electionArray[i][1];
+            if (electionArray[i][1] == 0) { 
+                send[electionId] = -2;
+                recv[electionId] = -2;
+            } else {
+                lastSeen = i;
+                send[electionId] = -1;
+                recv[electionId] = -1;
+            }
+            f << electionId << "=" << send[electionId] << ", ";
+        }
+        if (activeCount == 1) {
+            send[numberOfProcesses - 1] = lastSeen;
+        } else {
+            f << std::endl;
+            send[electionArray[processId][0]] = processId;
+            for (int i = 0; i < activeCount; i++) {
+                if (!active) continue;
+                MPI_Request req;
+                MPI_Status stat;
+                int tempSend[numberOfProcesses];
+                MPI_Isend(&send[0], numberOfProcesses, MPI_INT, next, 0, MPI_COMM_WORLD, &req);
+                MPI_Recv(&recv[0], numberOfProcesses, MPI_INT, last, 0, MPI_COMM_WORLD, nullptr);
+                MPI_Wait(&req, &stat); 
+                
+                f << "I sent: ";
+                for (int i = 0; i < numberOfProcesses; i++)
+                    f << "send[" << i << "]=" << send[i] << ", ";
+                f << std::endl;
+                f << "I recv: ";
+                for (int i = 0; i < numberOfProcesses; i++)
+                    f << "recv[" << i << "]=" << recv[i] << ", ";
+                f << std::endl;
 
+                // swap sendArray with recv
+                for (int i = 0; i < numberOfProcesses; i++) {
+                    tempSend[i] = send[i];
+                    send[i] = recv[i];
+                    recv[i] = tempSend[i];
+                }
+                send[electionArray[processId][0]] = processId;
+                f << "Finished run " << i << std::endl;
+            }
+            
+            for (int i = 0; i < numberOfProcesses; i++) {
+                f << "send[" << i << "]=" << send[i];
+                f << ", recv[" << i << "]=" << recv[i] << std::endl;;
+            }
+
+            // swap send with recv one more time.
+            int temp[numberOfProcesses];
+            for (int i = 0; i < numberOfProcesses; i++) {
+                temp[i] = recv[i];
+                recv[i] = send[i];
+                send[i] = temp[i];
+            }
+        }
+        int winnerNum = 0;
+        for (int i = numberOfProcesses; i > 0; i--) {
+            if (send[i - 1] != -2) {
+                // I WIN!
+                winnerNum = send[i - 1];
+                break;
+            }
+        }
         if(processId == 0)
         {
-            int winner = 0; // CHANGE THIS
+            int winner;
+            if (winnerNum != 0) {
+                MPI_Recv(&winner, 1, MPI_INT, winnerNum, 0, MPI_COMM_WORLD, nullptr); 
+            }
             PrintElectionResult(winner, round, numberOfProcesses, electionArray);
+        } else {
+            if (winnerNum == processId) {
+                f << "I WIN!" << std::endl;
+                MPI_Send(&processId, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            }
         }
     }
 
+    f.close();
     delete[] electionArray;
 
     MPI_Finalize();
